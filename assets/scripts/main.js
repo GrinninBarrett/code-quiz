35849f0c12c42f40b6a994dc8b428feca6544b28
Tucker Barrett <tuckerbarrett@gmail.com>
2021-06-28 15:37:38 -0400

    Moves assets to an assets folder, adds questions.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -43,52 +43,6 @@ let allScores = [];
 let nextScore;
 
 
-//Create array of objects containing all questions and their answers
-let questions = [
-
-    {
-        question: "What is the official name of JavaScript?",
-        correctAnswer: "ECMAScript",
-        wrongOne: "TypeScript",
-        wrongTwo: "CoffeeScript",
-        wrongThree: "Java",
-    },
-
-    {
-        question: "Inside which HTML element do we link the JavaScript?",
-        correctAnswer: "<script>",
-        wrongOne: "<javascript>",
-        wrongTwo: "<scripting>",
-        wrongThree: "<js>",
-    },
-
-    {
-        question: `How do you write "Hello, World" in an alert box?`,
-        correctAnswer: `alert("Hello, World");`,
-        wrongOne: `alertBox("Hello, World");`,
-        wrongTwo: `console.log("Hello, World");`,
-        wrongThree: `msg("Hello, World");`,
-    },
-
-    {
-        question: `What method is used to add an element to a JavaScript array?`,
-        correctAnswer: `push()`,
-        wrongOne: `pop()`,
-        wrongTwo: `concat()`,
-        wrongThree: `splice()`,
-    },
-
-    {
-        question: `Which of these is a correct way to declare a function?`,
-        correctAnswer: `function funkyTown() {};`,
-        wrongOne: `let funkyTown = [];`,
-        wrongTwo: `var funkyTown = function {};`,
-        wrongThree: `function.funkyTown = () {}`,
-    }
-
-];
-
-
 //Add event listeners to buttons (view high scores, start, answer choices, initials submit)
 viewScoresButton.addEventListener("click", function() {
     if (!onHighScoresPage && !gameInProgress) {
---
+++ assets/scripts/questions.js
@@ -0,0 +1,44 @@
+//Create array of objects containing all questions and their answers
+let questions = [
+
+    {
+        question: "What is the official name of JavaScript?",
+        correctAnswer: "ECMAScript",
+        wrongOne: "TypeScript",
+        wrongTwo: "CoffeeScript",
+        wrongThree: "Java",
+    },
+
+    {
+        question: "Inside which HTML element do we link the JavaScript?",
+        correctAnswer: "<script>",
+        wrongOne: "<javascript>",
+        wrongTwo: "<scripting>",
+        wrongThree: "<js>",
+    },
+
+    {
+        question: `How do you write "Hello, World" in an alert box?`,
+        correctAnswer: `alert("Hello, World");`,
+        wrongOne: `alertBox("Hello, World");`,
+        wrongTwo: `console.log("Hello, World");`,
+        wrongThree: `msg("Hello, World");`,
+    },
+
+    {
+        question: `What method is used to add an element to a JavaScript array?`,
+        correctAnswer: `push()`,
+        wrongOne: `pop()`,
+        wrongTwo: `concat()`,
+        wrongThree: `splice()`,
+    },
+
+    {
+        question: `Which of these is a correct way to declare a function?`,
+        correctAnswer: `function funkyTown() {};`,
+        wrongOne: `let funkyTown = [];`,
+        wrongTwo: `var funkyTown = function {};`,
+        wrongThree: `function.funkyTown = () {}`,
+    }
+
+];
